4c66cf489cb4f807ad7c25602c2f7775fb34c6aa
haogege <haogege@users.noreply.github.com>
2022-06-20 14:17:40 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -116,7 +116,7 @@ function GenerateRules() {
                 # "https://dns.alidns.com:443/dns-query"
                 # "https://dns.ipv6dns.com:443/dns-query"
                 # "https://doh.360.cn:443/dns-query"
-                "https://doh.pub:443/dns-query"
+                "https://120.53.53.53/dns-query"
                 # "tls://dns.alidns.com:853"
                 # "tls://dns.ipv6dns.com:853"
                 # "tls://dot.360.cn:853"
@@ -124,7 +124,7 @@ function GenerateRules() {
             )
             foreign_dns=(
                 # "https://dns.google:443/dns-query"
-                "https://dns.opendns.com:443/dns-query"
+                "https://1.0.0.1/dns-query"
                 # "https://dns11.quad9.net:443/dns-query"
                 # "https://dns64.dns.google:443/dns-query"
                 # "tls://dns.google:853"
